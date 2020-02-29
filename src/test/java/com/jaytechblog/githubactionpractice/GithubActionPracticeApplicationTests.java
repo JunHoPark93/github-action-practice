b754963fc0539b9d097b58cfd15654951afd9a75
JunHoPark93 <pjh374968@gmail.com>
2020-02-29 16:36:16 +0900

    Initial commit

---
+++ src/test/java/com/jaytechblog/githubactionpractice/GithubActionPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaytechblog.githubactionpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
