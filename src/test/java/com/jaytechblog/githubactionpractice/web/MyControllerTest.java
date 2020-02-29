bd09fc4da5f8450631d2aaf8d275b03aa8684a9b
JunHoPark93 <pjh374968@gmail.com>
2020-02-29 18:13:12 +0900

    test 추가

--- src/test/java/com/jaytechblog/githubactionpractice/web/MyControllerTest.java
+++ src/test/java/com/jaytechblog/githubactionpractice/web/MyControllerTest.java
@@ -1,10 +1,32 @@
 package com.jaytechblog.githubactionpractice.web;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
+import java.util.Objects;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
 class MyControllerTest {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
     @Test
     void name() {
-        System.out.println("Github action");
+        webTestClient.get().uri("/")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .consumeWith(res -> {
+                    String body = new String(Objects.requireNonNull(res.getResponseBody()));
+                    System.out.println(">>>>>>>>>>>>>>>" + body);
+                    assertThat(body.contains("hi")).isTrue();
+                });
     }
 }
